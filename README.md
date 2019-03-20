fa6bf56609082cccd9025d414d7e2e83aea9267b
Joe Jarvis <joe.jarvis109@gmail.com>
2019-03-19 20:38:16 -0400

    Initial Version

---
+++ README.md
@@ -0,0 +1,14 @@
+# Personal Podcast
+
+How to use:
+ * Set the variables in the options
+   * `MAX_ITEMS`: the number of items to keep in the feed (20)
+   * `DELETE_OLD`: whether to delete oldest items when exceeding `MAX_ITEMS` (True)
+   * `PODCAST_DIR`: A folder on your computer with podcast audio (/var/www/html/podcast)
+   * `SERVER`: `PODCAST_DIR`, but accessable on the web (http://your-site.com/podcast)
+   * `TITLE`: The title of the podcast (Personal Podcast)
+   * `DESC`: The description of the podcast (A personal podcast)
+   * `LOGO`: An icon to use as a logo (http://placekitten.com/500/500)
+ * Place audio in `PODCAST_DIR`
+ * Run `personal-podcast.py` (manually, or by cron)
+ * A file called `podcast.rss` will be created in `PODCAST_DIR`
---
+++ personal-podcast.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import os
+from feedgen.feed import FeedGenerator
+
+###########
+# OPTIONS #
+###########
+
+# maximum number of items to keep
+MAX_ITEMS = 20
+# whether to delete old items
+DELETE_OLD = True
+# location of audio files
+PODCAST_DIR = "/var/www/html/podcast/"
+# location of PODCAST_DIR on the internet
+SERVER = "http://your-site.com/podcast/"
+# title of podcast
+TITLE = "Personal Podcast"
+# description of podcast
+DESC = "A personal podcast"
+# podcast logo, as a url
+LOGO = "http://placekitten.com/500/500"
+
+os.chdir(PODCAST_DIR)
+items = filter(lambda x: os.path.isfile(x) and x.endswith(".mp3"), os.listdir())
+items = [f for f in items]
+# sort by date, newest to oldest
+items.sort(key=os.path.getmtime, reverse=True)
+
+# delete items above max
+if DELETE_OLD:
+    for delete in items[MAX_ITEMS:]:
+        print("DELETING: " + delete)
+        os.remove(delete)
+
+# keep only MAX_ITEMS newest items, reverse to be oldest to newest
+items = items[:MAX_ITEMS][::-1]
+
+fg = FeedGenerator()
+fg.title(TITLE)
+fg.description(DESC)
+fg.link(href=SERVER + "podcast.rss", rel="self")
+fg.load_extension("podcast")
+
+# add entries
+for item in items:
+    fe = fg.add_entry()
+    url = SERVER + item
+    fe.id(url)
+    fe.title(item)
+    fe.description(url)
+    fe.enclosure(url, 0, "audio/mpeg")
+
+# export to rss
+fg.rss_str(pretty=True)
+fg.rss_file("podcast.rss")
